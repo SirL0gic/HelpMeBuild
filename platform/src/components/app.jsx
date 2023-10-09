04a94dd1ab40a17dc70292a5e4a9a01d3d719b16
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-10-09 13:54:25 +0400

    Init next

---
+++ platform/src/components/app.jsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css"
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout;
